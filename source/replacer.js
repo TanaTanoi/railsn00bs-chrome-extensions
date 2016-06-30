7027a6a8f5bdd009307c8de1f59dab2f74a5c622
Tana Tanoi <tana.tanoi@gmail.com>
2016-06-30 20:34:18 +1200

    Version two of basic app

--- source/replacer.js
+++ source/replacer.js
@@ -1,3 +1,9 @@
-	$("body").append(
-		"<img width=500 src=\"https://media.giphy.com/media/kJ04xPBmQp9Ti/giphy.gif\">"
-	);
+$.ajax({
+    url: "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=pizza+cat",
+    success: function(result) {
+        image_url = result.data.image_url;
+				$("body").append(
+					`<div class='box'><img width=500 src="${image_url}"></div>`
+				);
+    }
+});
